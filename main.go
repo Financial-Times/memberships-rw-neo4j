7d1b092fa1de516ea1c4e7d5579b4048b4675331
Scott Newton <scott.newton@ft.com>
2016-09-07 10:29:22 +0100

    Refactored app to use neo utils to connect to neo4j

--- main.go
+++ main.go
@@ -11,8 +11,6 @@ import (
 	"github.com/Financial-Times/neo-utils-go/neoutils"
 	log "github.com/Sirupsen/logrus"
 	"github.com/jawher/mow.cli"
-	"github.com/jmcvetta/neoism"
-	"net/http"
 )
 
 func main() {
@@ -60,14 +58,15 @@ func main() {
 	})
 
 	app.Action = func() {
-		db, err := neoism.Connect(*neoURL)
-		db.Session.Client = &http.Client{Transport: &http.Transport{MaxIdleConnsPerHost: 100}}
+		conf := neoutils.DefaultConnectionConfig()
+		conf.BatchSize = *batchSize
+		db, err := neoutils.Connect(*neoURL, conf)
+
 		if err != nil {
 			log.Errorf("Could not connect to neo4j, error=[%s]\n", err)
 		}
 
-		batchRunner := neoutils.NewBatchCypherRunner(neoutils.StringerDb{db}, *batchSize)
-		membershipsDriver := memberships.NewCypherDriver(batchRunner, db)
+		membershipsDriver := memberships.NewCypherMembershipService(db)
 		membershipsDriver.Initialise()
 
 		baseftrwapp.OutputMetricsIfRequired(*graphiteTCPAddress, *graphitePrefix, *logMetrics)
@@ -78,7 +77,7 @@ func main() {
 
 		var checks []v1a.Check
 		for _, service := range services {
-			checks = append(checks, makeCheck(service, batchRunner))
+			checks = append(checks, makeCheck(service, db))
 		}
 
 		baseftrwapp.RunServerWithConf(baseftrwapp.RWConf{
